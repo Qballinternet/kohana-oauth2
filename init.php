21d716e188b547ca2a4730f6a97a86c977ea55f7
Maurice <develop@qball.nl>
2012-04-12 14:53:34 +0200

    Default route PSR-0 awareness

--- init.php
+++ init.php
@@ -2,6 +2,6 @@
 
 Route::set('oauth2', 'oauth2/<action>')
 	->defaults(array(
-		'directory'  => 'oauth2',
-		'controller' => 'endpoints',
+		'directory'  => 'OAuth2',
+		'controller' => 'Endpoints',
 	));
